357c343f919731db979833ecb493ef80156e5bb7
eduvf <155389249+eduvf@users.noreply.github.com>
2024-06-29 18:39:51 +0200

    add parse_assign

--- baya.c
+++ baya.c
@@ -1,12 +1,37 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TOK_LEN 16
 
 FILE *f;
 char t[TOK_LEN];
+
+int isnum(long *num) {
+  *num = strtol(t, NULL, 0);
+  return errno;
+}
+
+char isreg() {
+  if (strcmp(t, "x") == 0) return 'x';
+  if (strcmp(t, "y") == 0) return 'y';
+  if (strcmp(t, "z") == 0) return 'z';
+  if (strcmp(t, "w") == 0) return 'w';
+  return 0;
+}
+
+char isop() {
+  if (strcmp(t, "=") == 0) return '=';
+  if (strcmp(t, "+=") == 0) return '+';
+  if (strcmp(t, "-=") == 0) return '-';
+  if (strcmp(t, "*=") == 0) return '*';
+  if (strcmp(t, "/=") == 0) return '/';
+  if (strcmp(t, "%=") == 0) return '%';
+  return 0;
+}
 
 char *scan() {
   char c;
@@ -28,9 +53,41 @@ char *scan() {
   return NULL;
 }
 
+char *next() {
+  if (scan() != NULL) return t;
+  exit(1);
+}
+
+void parse_assign() {
+  char reg;
+  char op;
+  long num;
+  char from_reg;
+
+  reg = isreg();
+  next();
+  if (!(op = isop())) exit(1);
+
+  next();
+  if ((from_reg = isreg())) {
+    printf("r%c op %c with r%c\n", reg, op, from_reg);
+    return;
+  }
+
+  if (op != '=' && op != '+') exit(1);
+  if (isnum(&num) != 0) exit(1);
+
+  printf("r%c op %c with 0x%lx\n", reg, op, num);
+  return;
+}
+
+void parse_print() {}
+void parse_loop() {}
+void parse_if() {}
+
 void parse() {
   while (scan() != NULL) {
-    printf("%s\n", t);
+    if (isreg()) parse_assign();
   }
 }
 
